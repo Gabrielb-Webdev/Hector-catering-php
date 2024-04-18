8ce14faa0299820c856ab0c06ad0d1f095d1fe86
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-04-18 13:13:23 -0300

    Done

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,16 +1,19 @@
 <!-- backend/conexion.php -->
 
 <?php
+if(session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Datos de conexión a la base de datos
 $servername = "roundhouse.proxy.rlwy.net";
 $username = "root";
 $password = "MKIacdLxZxrjnYHNMGyhQtekMghFKlGq";
 $database = "railway";
-$port = "12331";
+$db_port = "12331";
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $database, $port);
+$conn = new mysqli($servername, $username, $password, $database, $db_port);
 
 // Verificar la conexión
 if ($conn->connect_error) {
